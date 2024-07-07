99b125642416474750df608547f0641c8df9dc5f
Jun Anthony <Unknown@DESKTOP-LAR4J4U>
2024-07-07 12:57:40 +1200

    Add project files.

---
+++ Invoice2-MobileMekaniko/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Invoice2_MobileMekaniko.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
